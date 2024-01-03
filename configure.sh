66b5c3ae91d87784df5d3d64ace3c3dae5d897e1
reticulardev <reticulardev@outlook.com>
2024-01-03 13:34:44 -0300

    MPX

--- configure.sh
+++ configure.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-if [ $# -gt 0 ] && [ $1 = "--update" ]; then
-  echo "Updating..."
-  rm -rf src/pysidex/
+if [ ! -d venv/ ]; then
+  python3 -m venv venv
 fi
 
-if [ -d src/pysidex/ ]; then
+if [ $# -gt 0 ] && [ $1 = "--update" ]; then
+  echo "Updating..."
+  rm -rf src/MPX/pysidex/
+fi
+
+if [ -d src/MPX/pysidex/ ]; then
   echo "It has already been configured!"
 else
-  mkdir -p src/pysidex/
-  git clone https://github.com/reticulardev/pysidex.git src/pysidex/
-  . ../venv/bin/activate && python -m pip install --upgrade pip && python -m pip install -r src/pysidex/requirements.txt
-  echo
-  echo "Done! To test use:"
-  echo "python src/pysidex/src/demo.py"
+  mkdir -p src/MPX/pysidex/
+  git clone https://github.com/reticulardev/pysidex.git src/MPX/pysidex/
+  rm -rf src/MPX/pysidex/.git/
+  . venv/bin/activate && python -m pip install --upgrade pip && python -m pip install -r src/MPX/pysidex/requirements.txt
+  echo "..."
+  echo "Done! To test run: python src/MPX/pysidex/src/demo.py"
 fi
